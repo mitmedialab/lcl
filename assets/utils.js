8be09b4ea5ddf1de917dfab30a13ed8b4f29dcbe
Carmelo Presicce <tarmelop@media.mit.edu>
2022-03-10 12:38:53 -0500

    adding calendar to landing page

--- assets/utils.js
+++ assets/utils.js
@@ -3,6 +3,17 @@
  *
  */
 
+calendar_src = 'Nm8xaHEwYjRjZWk1bWo5bnVtOGhobW9kZWNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ'
+start_date = '20220418'
+end_date = '20220531'
+view_mode = 'WEEK' /* alternative can be MONTH */
+
+iframe_html = '<iframe id="calendar-iframe" src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=2&amp;bgcolor=%23ffffff' 
+		+'&amp;ctz=' + encodeURIComponent(getCurrentTimeZone()) + '&amp;src=' + calendar_src 
+		+ '&amp;color=%239E69AF&amp;mode='+ view_mode +'&amp;showPrint=0&amp;showCalendars=0&amp;dates='+start_date+'%2F'+ end_date 
+		+'" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>'
+
+
 function getCurrentTimeZone(){
 	return Intl.DateTimeFormat().resolvedOptions().timeZone || "America/New_York";
 }
@@ -20,6 +31,7 @@ $(document).ready(function(){
 		//alert(this.value);
 		console.log(this.value);
 		$('#timezone-header').text(this.value);
-		$('#calendar-iframe').attr('src', 'https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%23ffffff&ctz=' + encodeURIComponent(this.value) + '&src=OTNtdWFuNTVwbTQzamtvYXM3YnZvYWNwMDBAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%239E69AF&mode=WEEK&dates=20201019%2F20201129');
+		$('#calendar-iframe').attr('src', 'https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%23ffffff&ctz=' + 
+			encodeURIComponent(this.value) + '&src=' + calendar_src + '&color=%239E69AF&mode='+ view_mode +'&showPrint=0&showCalendars=0&dates=' + start_date +'%2F' + end_date);
 	});
 })
